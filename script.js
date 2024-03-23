71767a0924fd990be8200f31505a6f963dffb1c5
AnkitAdhikari <ankitadhikari31512@gmail.com>
2024-03-24 02:17:31 +0545

    add logic with basic style for remaining main sections and add faq section and footer styles yet to apply

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ emailContainer.addEventListener("click", () => {
   emailInput.focus();
 });
 
+const questions = document.querySelectorAll('.question-container');
+
 emailInput.addEventListener("focus", () => {
   emailContainer.style.setProperty("--beforeHeight", "70%");
   emailContainer.style.setProperty("--beforeFont", "1.2rem");
@@ -22,3 +24,12 @@ emailInput.addEventListener("focus", () => {
 emailInput.addEventListener("focusout", () => {
   emailContainer.style = defalutEmailContainerOverlay;
 });
+
+questions.forEach((question) => {
+  question.addEventListener('click', (e) => {
+    const number = e.currentTarget.dataset.questionnum;
+    document.querySelector(`.answer${number}`).classList.toggle('hidden');
+    const icon = e.currentTarget.querySelector('ion-icon');
+    console.log(icon.getAttribute('name') == "add-outline" ? icon.setAttribute('name', 'close-outline') : icon.setAttribute('name', 'add-outline'))
+  })
+})
